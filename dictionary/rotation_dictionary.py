cad4b36f51057bb5145639323ab8d0646eb973ad
dotorl127 <dotorl127@gmail.com>
2022-10-26 11:17:42 +0900

    update rotation dictionary

--- dictionary/rotation_dictionary.py
+++ dictionary/rotation_dictionary.py
@@ -3,18 +3,24 @@ from pyquaternion import Quaternion as Q
 
 cam_rot = {'kitti': {'waymo': Q(axis=[0, 1, 0], angle=np.pi / 2).rotation_matrix @
                               Q(axis=[0, 0, 1], angle=-np.pi / 2).rotation_matrix,
-                     'nuscenes': np.eye(3, dtype=np.float32)},
+                     'nuscenes': np.eye(3, dtype=np.float32),
+                     'udacity': np.eye(3, dtype=np.float32)},
            'waymo': {'kitti': Q(axis=[1, 0, 0], angle=np.pi / 2).rotation_matrix @
                               Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix,
                      'nuscenes': Q(axis=[1, 0, 0], angle=np.pi / 2).rotation_matrix @
-                                 Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix},
+                                 Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix,
+                     'udacity': np.eye(3, dtype=np.float32)},
            'nuscenes': {'kitti': np.eye(3, dtype=np.float32),
                         'waymo': Q(axis=[0, 1, 0], angle=np.pi / 2).rotation_matrix @
-                                 Q(axis=[0, 0, 1], angle=-np.pi / 2).rotation_matrix}}
+                                 Q(axis=[0, 0, 1], angle=-np.pi / 2).rotation_matrix,
+                        'udacity': np.eye(3, dtype=np.float32)}}
 
 lid_rot = {'kitti': {'waymo': np.eye(3, dtype=np.float32),
-                     'nuscenes': Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix},
+                     'nuscenes': Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix,
+                     'udacity': np.eye(3, dtype=np.float32)},
            'waymo': {'kitti': np.eye(3, dtype=np.float32),
-                     'nuscenes': Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix},
+                     'nuscenes': Q(axis=[0, 0, 1], angle=np.pi / 2).rotation_matrix,
+                     'udacity': np.eye(3, dtype=np.float32)},
            'nuscenes': {'kitti': Q(axis=[0, 0, 1], angle=-np.pi / 2).rotation_matrix,
-                        'waymo': Q(axis=[0, 0, 1], angle=-np.pi / 2).rotation_matrix}}
+                        'waymo': Q(axis=[0, 0, 1], angle=-np.pi / 2).rotation_matrix,
+                        'udacity': np.eye(3, dtype=np.float32)}}
